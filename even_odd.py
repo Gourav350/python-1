dc59de8208af72b2c0669cff3b92701f3fe052b9
Gourav350 <jangragourav350@gmail.com>
2025-09-15 20:25:23 +0530

    find even and odd number in pyhton
    find even and odd number in pyhton

---
+++ even_odd.py
@@ -0,0 +1,10 @@
+# Even or Odd Number Checker
+
+# Taking input from the user
+num = int(input("Enter a number: "))
+
+# Checking if number is even or odd
+if num % 2 == 0:
+    print(f"{num} is Even.")
+else:
+    print(f"{num} is Odd.")
