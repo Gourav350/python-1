64df14095774f3e90c114715639b30933a3daa56
Gourav350 <jangragourav350@gmail.com>
2025-09-30 19:05:37 +0530

    Add files via upload
    data type in python

---
+++ datatype.py
@@ -0,0 +1,15 @@
+num1=3
+num2=4.3
+sum=num1+num2
+product=num1*num2
+print(sum)
+print(product)
+
+num1 = 5
+print(num1, 'is of type', type(num1))
+
+num2 = 2.0
+print(num2, 'is of type', type(num2))
+
+num3 = 1+2j
+print(num3, 'is of type', type(num3))
