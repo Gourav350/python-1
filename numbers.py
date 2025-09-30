8d6ce8aaa87cf1450d59ea00e8e9d0a59223ac88
Gourav350 <jangragourav350@gmail.com>
2025-09-30 19:04:07 +0530

    Add files via upload
    numbers in pyhton

---
+++ numbers.py
@@ -0,0 +1,18 @@
+
+a = 10
+b = -25
+c = 0
+
+print(a, type(a))   
+print(b, type(b))   
+print(c, type(c))   
+
+x = 15
+y = 4.19
+print(x - y)   
+print(x * y)   
+print(x // y)  
+print(x % y)   
+print(x ** y)  
+
+print(x + y)   
